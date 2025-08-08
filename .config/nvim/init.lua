26d85ff09049870020726af18ef7848e5ccf2532
Adrian Javier <agjavier@kld.edu.ph>
2025-08-08 23:11:43 +0800

    disable broken neovim plugin configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,9 +5,9 @@ require "Lazy"
 vim.opt.termguicolors = true
 vim.opt.list = true
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#212529 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#343A40 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#495057 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#6C757D gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#ADB5BD gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#CED4DA gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#212529 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#343A40 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#495057 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#6C757D gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#ADB5BD gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#CED4DA gui=nocombine]]
--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,15 +1,17 @@
-local M = {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = {
-        char = "▏",
-        space_char_blankline = " ",
-        show_trailing_blankline_indent = true,
-        show_current_context = false,
-        show_current_context_start = false,
-        space_char_highlight_list = {
-            "IndentBlanklineIndent3"
-        },
-    },
-}
-
-return M
+-- local M = {
+--     "lukas-reineke/indent-blankline.nvim",
+-- 	main = "ibl",
+--     opts = {
+--         indent = { char = "▏" },
+--         space_char_blankline = " ",
+--         show_trailing_blankline_indent = true,
+--         show_current_context = false,
+--         show_current_context_start = false,
+--         space_char_highlight_list = {
+--             "IndentBlanklineIndent3"
+--         },
+--     },
+-- }
+-- 
+-- return M
+return {}
